ca142d9fb81fb9f547817f058b009ef81ae9b719
Ondra Pelech <ondra.pelech@gmail.com>
2020-09-02 12:46:31 +0200

    trait DecentScala

--- src/main/scala/com/github/sideeffffect/sbtdecentscala/DecentScalaPlugin.scala
+++ src/main/scala/com/github/sideeffffect/sbtdecentscala/DecentScalaPlugin.scala
@@ -17,36 +17,44 @@ object DecentScalaPlugin extends AutoPlugin {
 
   override def trigger: PluginTrigger = allRequirements
 
-  lazy val decentScalaSettings: List[Def.Setting[_]] =
-    List(
-      libraryDependencies ++= List(
-        compilerPlugin(Dependencies.betterMonadicFor),
-        compilerPlugin(Dependencies.kindProjector),
-        compilerPlugin(Dependencies.silencer),
-        Dependencies.silencerLib,
-      ),
-      semanticdbEnabled := true, // enable SemanticDB
-      semanticdbVersion := scalafixSemanticdb.revision, // use Scalafix compatible version
-      ThisBuild / scalafixScalaBinaryVersion := CrossVersion.binaryScalaVersion(scalaVersion.value),
-      ThisBuild / scalafixDependencies ++= List(
-        Dependencies.organizeImports,
-        Dependencies.scaluzzi,
-      ),
-      scalacOptions ++= List(
-        "-P:silencer:checkUnused",
-        "-Ywarn-macros:after",
-      ),
-      scalacOptions --= {
-        if (!sys.env.contains("CI"))
-          List("-Xfatal-warnings") // to enable Scalafix
-        else
-          List()
-      },
-    ) ++
-      addCommandAlias("check", "; lint; +missinglinkCheck; +mimaReportBinaryIssues; +test") ++
-      addCommandAlias(
-        "lint",
-        "; scalafmtSbtCheck; scalafmtCheckAll; compile:scalafix --check; test:scalafix --check",
+  object autoImport {
+
+    object DecentScala extends DecentScala
+
+  }
+
+  trait DecentScala {
+    lazy val decentScalaSettings: List[Def.Setting[_]] =
+      List(
+        libraryDependencies ++= List(
+          compilerPlugin(Dependencies.betterMonadicFor),
+          compilerPlugin(Dependencies.kindProjector),
+          compilerPlugin(Dependencies.silencer),
+          Dependencies.silencerLib,
+        ),
+        semanticdbEnabled := true, // enable SemanticDB
+        semanticdbVersion := scalafixSemanticdb.revision, // use Scalafix compatible version
+        ThisBuild / scalafixScalaBinaryVersion := CrossVersion.binaryScalaVersion(scalaVersion.value),
+        ThisBuild / scalafixDependencies ++= List(
+          Dependencies.organizeImports,
+          Dependencies.scaluzzi,
+        ),
+        scalacOptions ++= List(
+          "-P:silencer:checkUnused",
+          "-Ywarn-macros:after",
+        ),
+        scalacOptions --= {
+          if (!sys.env.contains("CI"))
+            List("-Xfatal-warnings") // to enable Scalafix
+          else
+            List()
+        },
       ) ++
-      addCommandAlias("fix", "; compile:scalafix; test:scalafix; scalafmtSbt; scalafmtAll")
+        addCommandAlias("check", "; lint; +missinglinkCheck; +mimaReportBinaryIssues; +test") ++
+        addCommandAlias(
+          "lint",
+          "; scalafmtSbtCheck; scalafmtCheckAll; compile:scalafix --check; test:scalafix --check",
+        ) ++
+        addCommandAlias("fix", "; compile:scalafix; test:scalafix; scalafmtSbt; scalafmtAll")
+  }
 }
