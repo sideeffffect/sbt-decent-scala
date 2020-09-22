d75ef26aa64ce303a9a2ad66f13ef87671cc1473
Scala Steward <me@scala-steward.org>
2020-09-22 06:38:35 +0200

    Update organize-imports to 0.4.1

--- src/main/scala/com/github/sideeffffect/sbtdecentscala/Dependencies.scala
+++ src/main/scala/com/github/sideeffffect/sbtdecentscala/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
 
     val betterMonadicFor: String = "0.3.1"
     val kindProjector: String = "0.10.3"
-    val organizeImports: String = "0.4.0"
+    val organizeImports: String = "0.4.1"
     val scaluzzi: String = "0.1.12"
     val silencer: String = "1.7.1"
 
