770f215a6360b422feae53fd62abb714b9c5c66b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-01-15 18:48:35 +0100

    Update sbt-scalafix to 0.9.25 (#87)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val sbtMissinglink = "0.3.1"
     val sbtRewarn = "0.1.2"
     val sbtScalafmt = "2.4.2"
-    val sbtScalafix = "0.9.24"
+    val sbtScalafix = "0.9.25"
     val sbtTpolecat = "0.1.16"
     val scaluzzi = "0.1.16"
     val silencer = "1.7.1"
