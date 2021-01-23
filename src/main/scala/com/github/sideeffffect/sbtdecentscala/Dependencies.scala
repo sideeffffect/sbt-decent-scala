d0f6cb7c37b801fb7419dcfea0b217b1bd3335fb
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-01-23 20:40:14 +0100

    Update scaluzzi to 0.1.17 (#88)

--- src/main/scala/com/github/sideeffffect/sbtdecentscala/Dependencies.scala
+++ src/main/scala/com/github/sideeffffect/sbtdecentscala/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val betterMonadicFor: String = "0.3.1"
     val kindProjector: String = "0.10.3"
     val organizeImports: String = "0.4.4"
-    val scaluzzi: String = "0.1.16"
+    val scaluzzi: String = "0.1.17"
     val silencer: String = "1.7.1"
 
   }
