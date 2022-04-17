afbea5e2e33bb211ebb79a51577471d8922e22b1
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-04-17 22:35:01 +0200

    Update sbt-tpolecat to 0.2.3 (#219)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val sbtRewarn = "0.1.3"
     val sbtScalafmt = "2.4.6"
     val sbtScalafix = "0.9.34"
-    val sbtTpolecat = "0.1.22"
+    val sbtTpolecat = "0.2.3"
     val scaluzzi = "0.1.21"
     val silencer = "1.7.8"
 
