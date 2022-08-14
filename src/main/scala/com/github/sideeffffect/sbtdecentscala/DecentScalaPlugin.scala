09fd66526e1ed971e084bd60448a36418e96dc78
Ondra Pelech <ondra.pelech@gmail.com>
2022-08-14 21:50:48 +0200

    Update Scala versions

--- src/main/scala/com/github/sideeffffect/sbtdecentscala/DecentScalaPlugin.scala
+++ src/main/scala/com/github/sideeffffect/sbtdecentscala/DecentScalaPlugin.scala
@@ -29,9 +29,9 @@ object DecentScalaPlugin extends AutoPlugin {
   }
 
   trait DecentScala {
-    def decentScalaVersion3 = "3.1.2"
+    def decentScalaVersion3 = "3.1.3"
     def decentScalaVersion213 = "2.13.8"
-    def decentScalaVersion212 = "2.12.15"
+    def decentScalaVersion212 = "2.12.16"
     def decentScalaVersion211 = "2.11.12"
     def decentScalaSettings: List[Def.Setting[_]] =
       List(
