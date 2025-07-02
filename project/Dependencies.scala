c6a6968b08fc8d6c9eb9c0cc8e8627af3aacfe10
Scala Steward <scala_steward@virtuslab.com>
2025-07-02 04:20:39 +0000

    Update sbt-scalafmt to 2.5.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val sbtDynver = "5.1.1"
     val sbtMissinglink = "0.3.6"
     val sbtRewarn = "0.1.3"
-    val sbtScalafmt = "2.5.4"
+    val sbtScalafmt = "2.5.5"
     val sbtScalafix = "0.14.3"
     val sbtTpolecat = "0.5.2"
     val sbtVersionPolicy = "3.2.1"
