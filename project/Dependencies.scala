fe320d57f584a8d8f2da4e5e942f853e4a2f1fd2
Scala Steward <scala_steward@virtuslab.com>
2025-10-11 18:27:47 +0000

    Update sbt-scalafix to 0.14.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val sbtMissinglink = "0.3.6"
     val sbtRewarn = "0.1.3"
     val sbtScalafmt = "2.5.5"
-    val sbtScalafix = "0.14.3"
+    val sbtScalafix = "0.14.4"
     val sbtTpolecat = "0.5.2"
     val sbtVersionPolicy = "3.2.1"
     val scaluzzi = "0.1.23"
